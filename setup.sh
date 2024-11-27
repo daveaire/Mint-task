e317fcb0be924448d9f54158e43d8070807a7192
daveaire <airebamenosomurie@gmail.com>
2024-11-27 11:05:11 +0000

    removed dependencies

--- setup.sh
+++ setup.sh
@@ -1,51 +1,15 @@
 #!/bin/bash
 
+# Script to configure Minikube and deploy applications using Terraform
+
 set -e  # Exit on error
 
-# Function to install dependencies
-install_dependencies() {
-  echo "Updating system and installing prerequisites..."
-  sudo apt-get update -y
-  sudo apt-get upgrade -y
-  sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-
-  echo "Installing KVM and dependencies..."
-  sudo apt-get install -y qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils
-  sudo apt-get install -y virt-manager
-
-  echo "Adding user to the 'libvirt' group..."
-  sudo usermod -aG libvirt $USER
-
-  echo "Installing Minikube..."
-  curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-  sudo install minikube-linux-amd64 /usr/local/bin/minikube
-  rm minikube-linux-amd64
-
-  echo "Installing kubectl..."
-  curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-  sudo install kubectl /usr/local/bin/kubectl
-  rm kubectl
-
-  echo "Installing Helm 3..."
-  curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
-  sudo apt-get install apt-transport-https --yes
-  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
-  sudo apt-get update
-  sudo apt-get install -y helm
-
-  echo "Installing Terraform..."
-  curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-  sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-  sudo apt-get update -y
-  sudo apt-get install -y terraform
-}
-
-# Function to configure and start Minikube with KVM2 driver
+# Function to configure and start Minikube with Docker driver
 configure_minikube() {
-  echo "Configuring Minikube with KVM2 driver..."
+  echo "Configuring Minikube with Docker driver..."
   minikube config set cpus 2
   minikube config set memory 4096
-  minikube config set driver kvm2
+  minikube config set driver docker
 
   echo "Starting Minikube..."
   minikube start
@@ -89,7 +53,6 @@ verify_deployment() {
 
 # Main script execution
 echo "Starting the automation script..."
-install_dependencies
 configure_minikube
 add_helm_repo
 deploy_terraform
